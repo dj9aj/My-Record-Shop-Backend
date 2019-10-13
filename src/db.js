c62d6c32a59cac97abcb728bbcaa199dbe64dbb9
Jack Doyle <jackdo@hotmail.co.uk>
2019-10-13 18:14:35 +0100

    Change filepath in generated folder.

--- src/db.js
+++ src/db.js
@@ -1,7 +1,7 @@
 const { prisma } = require('prisma-binding');
 
 const db = new prisma({ 
-  typeDefs: 'src/generated/prisma.graphql/prisma.graphql',
+  typeDefs: 'src/generated/prisma/prisma.graphql',
   endpoint: process.env.PRISMA_ENDPOINT, 
   secret: process.env.PRISMA_SECRET,
   debug: false,
