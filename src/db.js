e1246f96c005e5731d3fe5d0c370d7126840ead6
Jack Doyle <jackdo@hotmail.co.uk>
2019-10-23 11:08:47 +0100

    Use env vars for prisma endpoint.

--- src/db.js
+++ src/db.js
@@ -2,8 +2,7 @@ const { Prisma } = require('prisma-binding');
 
 const db = new Prisma({
   typeDefs: 'src/generated/prisma/prisma.graphql',
-  // endpoint: process.env.PRISMA_ENDPOINT,
-  endpoint: 'http://localhost:4466',
+  endpoint: process.env.PRISMA_ENDPOINT,
   secret: process.env.PRISMA_SECRET,
   debug: true,
 });
