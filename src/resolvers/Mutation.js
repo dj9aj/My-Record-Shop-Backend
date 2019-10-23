191e6ec5ed22d4c9dfe8520b950e964dda5c1674
Jack Doyle <jackdo@hotmail.co.uk>
2019-10-23 12:00:44 +0100

    Add createUser mutation.

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,3 +1,13 @@
-const Mutations = {};
+const Mutations = {
+  async createUser(parent, args, ctx, info) {
+    // Have access to db functionality through ctx.
+    const user = await ctx.db.mutation.createUser({
+      data: {
+        ...args,
+      },
+    }, info); // info variable will insure that the item is returned from the DB once it has been created
+    return user;
+  },
+};
 
 module.exports = Mutations;
