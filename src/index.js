20c825c188f0af53b1e18627fd206f509690cc59
Jack Doyle <jackdo@hotmail.co.uk>
2019-12-09 21:15:47 +0000

    Add code comment for cors setting.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 require('dotenv').config({ path: process.env.ENV_FILE || '.env' });
 const server = require('./createServer');
 
+// Only requests from frontend URL allowed
 const cors = { credentials: true, origin: process.env.FRONTEND_URL };
 
 server.start({ cors }, ({ port }) => console.log(`Server is now running on port http://localhost:${port}`));
